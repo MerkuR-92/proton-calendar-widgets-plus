2782248467a61ef07c5d0a7372ff1c73900dcc21
Benjamin Garcia <garciabenjamin@protonmail.com>
2021-09-22 09:59:22 +0200

    CALAND-1365: Handle delete standalone SE in disabled calendar

--- app/src/main/java/me/proton/android/calendar/domain/usecase/HandleDeleteUseCase.kt
+++ app/src/main/java/me/proton/android/calendar/domain/usecase/HandleDeleteUseCase.kt
@@ -359,8 +359,8 @@ class HandleDeleteUseCase( // TODO TESTS
         val handleDeleteResult = handleDelete(
             userId,
             event.id,
-            if (event.isRecurring() || (event.isSingleEdit() && event.calendar.isDisabled)) EventEditDeleteOption.ALL_EVENTS else EventEditDeleteOption.THIS_EVENT,
-            if (event.isRecurring() || (event.isSingleEdit() && event.calendar.isDisabled)) null else if (event.isSingleEdit()) occurrenceNumber else 0,
+            if (event.isRecurring() || (event.isSingleEdit() && !isStandaloneSingleEdit && event.calendar.isDisabled)) EventEditDeleteOption.ALL_EVENTS else EventEditDeleteOption.THIS_EVENT,
+            if (event.isRecurring() || (event.isSingleEdit() && !isStandaloneSingleEdit && event.calendar.isDisabled)) null else if (event.isSingleEdit()) occurrenceNumber else 0,
             !(event.isRecurring() && hasNonCancelledSingleEdit) || event.calendar.isDisabled,
             isStandaloneSingleEdit
         )
