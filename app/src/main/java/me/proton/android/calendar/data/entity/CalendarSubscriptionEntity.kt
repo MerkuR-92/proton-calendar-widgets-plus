60373c23c0aac2e59623d5fe2b4cff6f99754c14
Ben <garciabenjamin@protonmail.com>
2021-11-11 15:26:31 +0100

    CALAND-1499 & CALAND-1508: Display status syncing and display sync error details for subscribed calendar in settings.
    bump version

--- app/src/main/java/me/proton/android/calendar/data/entity/CalendarSubscriptionEntity.kt
+++ app/src/main/java/me/proton/android/calendar/data/entity/CalendarSubscriptionEntity.kt
@@ -33,5 +33,50 @@ data class CalendarSubscriptionEntity(
     val url: String
 ) {
     // Calendar is in sync if status is 0 AND has been updated in the last 12 hours
-    val isSynced: Boolean get() = status == 0 && ((System.currentTimeMillis() / 1000) - lastUpdateTime < Duration.ofHours(12).seconds)
+    val isSynced: Boolean get() = status == CalendarSubscriptionStatus.OK.value &&
+            ((System.currentTimeMillis() / 1000) - lastUpdateTime < Duration.ofHours(12).seconds)
+
+    val isSyncing: Boolean get() = status == CalendarSubscriptionStatus.SYNCING.value
+
+    val isLastSyncOld: Boolean get() = lastUpdateTime != 0 &&
+            ((System.currentTimeMillis() / 1000) - lastUpdateTime >= Duration.ofHours(12).seconds)
+}
+
+enum class CalendarSubscriptionStatus(val value: Int) {
+    /** OK */
+    OK(0),
+    /** Synchronizing */
+    SYNCING(7),
+    /** Generic Error */
+    ERROR(1),
+    /** Invalid ICS */
+    INVALID_ICS(2),
+    /** Calendar soft-deleted */
+    CALENDAR_SOFT_DELETED(3),
+    /** Calendar not found */
+    NOT_FOUND(4),
+    /** User does not exist */
+    USER_DOES_NOT_EXIST(5),
+    /** Remote calendar size exceeded the limit */
+    SIZE_EXCEED_LIMIT(6),
+    /** Calendar missing primary key */
+    MISSING_PRIMARY_KEY(8),
+    /** Remote calendar fetching generic error */
+    HTTP_REQUEST_FAILED_GENERIC_ERROR(20),
+    /** Remote calendar fetching bad request error */
+    HTTP_REQUEST_FAILED_BAD_REQUEST(21),
+    /** Remote calendar fetching unauthorized error */
+    HTTP_REQUEST_FAILED_UNAUTHORIZED(22),
+    /** Remote calendar fetching forbidden error */
+    HTTP_REQUEST_FAILED_FORBIDDEN(23),
+    /** Remote calendar fetching not found error */
+    HTTP_REQUEST_FAILED_NOT_FOUND(24),
+    /** Remote calendar fetching internal server error */
+    HTTP_REQUEST_FAILED_INTERNAL_SERVER_ERROR(25),
+    /** Remote calendar provider timeout */
+    HTTP_REQUEST_FAILED_TEST(26),
+    /** Internal Proton-Proton link not found */
+    P2P_LINK_NOT_FOUND(27),
+    /** Internal Proton-Proton remote calendar is not decryptable */
+    UNABLE_TO_DECRYPT(28)
 }
