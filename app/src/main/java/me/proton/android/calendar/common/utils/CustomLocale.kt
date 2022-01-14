0848d12644537e23cdecfcbc889ce360a43b1781
Ben <garciabenjamin@protonmail.com>
2022-01-14 10:46:55 +0100

    Fixed app theme by making sure we set the uiMode on creating new context with custom Locale.

--- app/src/main/java/me/proton/android/calendar/common/utils/CustomLocale.kt
+++ app/src/main/java/me/proton/android/calendar/common/utils/CustomLocale.kt
@@ -2,12 +2,10 @@ package me.proton.android.calendar.common.utils
 
 import android.content.Context
 import android.content.res.Configuration
-import android.content.res.Resources
-import android.os.Build
 import androidx.preference.PreferenceManager
+import me.proton.android.calendar.common.AppTheme
 import me.proton.android.calendar.common.FeatureFlag.CHANGE_LANGUAGE
 import me.proton.android.calendar.common.SharedPreferencesKeys
-import me.proton.android.calendar.common.logger.TimberLogger
 import me.proton.android.calendar.common.utils.DateTimeUtilsImpl.getLocaleForFormatting
 import java.util.*
 
@@ -24,18 +22,30 @@ object CustomLocale {
         var languageToSet = locale.substringBefore("-")
         var countryToSet = locale.substringAfter("-", "")
 
-        if (locale == "") { // go back to default
+        if (locale == "") {
+            // If settings are in Auto Detect, use System language if supported, or fallback to en-US
             val defaultSupportedLanguageTag = getLocaleForFormatting().toLanguageTag()
             languageToSet = defaultSupportedLanguageTag.substringBefore("-")
             countryToSet = defaultSupportedLanguageTag.substringAfter("-", "")
         }
 
+        // Create custom Locale
         val localeToSet = Locale(languageToSet, countryToSet)
         Locale.setDefault(localeToSet)
 
         val resources = context.resources
         val configuration = Configuration(resources.configuration)
+
+        // Set the custom locale in Configuration
         configuration.setLocale(localeToSet)
+
+        // Make sure we also set the app theme in Configuration
+        when (AppTheme.values()[PreferenceManager.getDefaultSharedPreferences(context).getInt(SharedPreferencesKeys.THEME, AppTheme.SYSTEM_DEFAULT.value)]) {
+            AppTheme.LIGHT -> configuration.uiMode = Configuration.UI_MODE_NIGHT_NO
+            AppTheme.DARK -> configuration.uiMode = Configuration.UI_MODE_NIGHT_YES
+            else -> configuration.uiMode = Configuration.UI_MODE_NIGHT_UNDEFINED
+        }
+
         val updatedContext = context.createConfigurationContext(configuration)
         return updatedContext ?: context
     }
