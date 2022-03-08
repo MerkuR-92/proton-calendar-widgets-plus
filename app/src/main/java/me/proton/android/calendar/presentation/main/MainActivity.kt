560f5ac751bb0a03f2dd3ae57ccbbb9fee59e463
Ben <garciabenjamin@protonmail.com>
2022-03-08 14:26:33 +0100

    Updated sidebar view selection text and icon colors

--- app/src/main/java/me/proton/android/calendar/presentation/main/MainActivity.kt
+++ app/src/main/java/me/proton/android/calendar/presentation/main/MainActivity.kt
@@ -809,9 +809,14 @@ class MainActivity : AppCompatActivity(), KoinComponent {
                     nav_view_main_content.nav_view_switcher_month_layout.background = null
 
                     // Set icon tint
-                    nav_view_main_content.nav_view_switcher_agenda_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_norm))
+                    nav_view_main_content.nav_view_switcher_agenda_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white))
                     nav_view_main_content.nav_view_switcher_day_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_weak))
                     nav_view_main_content.nav_view_switcher_month_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_weak))
+
+                    // Set text color
+                    nav_view_main_content.nav_view_switcher_agenda_title.setTextColor(ContextCompat.getColor(this, R.color.white))
+                    nav_view_main_content.nav_view_switcher_day_title.setTextColor(ContextCompat.getColor(this, R.color.text_norm))
+                    nav_view_main_content.nav_view_switcher_month_title.setTextColor(ContextCompat.getColor(this, R.color.text_norm))
                 }
                 ViewMode.DAY -> {
                     // Set selected background
@@ -821,8 +826,13 @@ class MainActivity : AppCompatActivity(), KoinComponent {
 
                     // Set icon tint
                     nav_view_main_content.nav_view_switcher_agenda_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_weak))
-                    nav_view_main_content.nav_view_switcher_day_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_norm))
+                    nav_view_main_content.nav_view_switcher_day_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white))
                     nav_view_main_content.nav_view_switcher_month_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_weak))
+
+                    // Set text color
+                    nav_view_main_content.nav_view_switcher_agenda_title.setTextColor(ContextCompat.getColor(this, R.color.text_norm))
+                    nav_view_main_content.nav_view_switcher_day_title.setTextColor(ContextCompat.getColor(this, R.color.white))
+                    nav_view_main_content.nav_view_switcher_month_title.setTextColor(ContextCompat.getColor(this, R.color.text_norm))
                 }
                 ViewMode.MONTH -> {
                     // Set selected background
@@ -833,7 +843,12 @@ class MainActivity : AppCompatActivity(), KoinComponent {
                     // Set icon tint
                     nav_view_main_content.nav_view_switcher_agenda_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_weak))
                     nav_view_main_content.nav_view_switcher_day_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_weak))
-                    nav_view_main_content.nav_view_switcher_month_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.sidebar_icon_norm))
+                    nav_view_main_content.nav_view_switcher_month_icon.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white))
+
+                    // Set text color
+                    nav_view_main_content.nav_view_switcher_agenda_title.setTextColor(ContextCompat.getColor(this, R.color.text_norm))
+                    nav_view_main_content.nav_view_switcher_day_title.setTextColor(ContextCompat.getColor(this, R.color.text_norm))
+                    nav_view_main_content.nav_view_switcher_month_title.setTextColor(ContextCompat.getColor(this, R.color.white))
                 }
             }
         })
