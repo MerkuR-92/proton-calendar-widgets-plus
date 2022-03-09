b52735ad9e1fe29bb9664a9d541861c5a3edd68a
Ben <garciabenjamin@protonmail.com>
2022-03-09 11:52:10 +0100

    Enabled delete calendar

--- app/src/main/java/me/proton/android/calendar/common/Constants.kt
+++ app/src/main/java/me/proton/android/calendar/common/Constants.kt
@@ -87,7 +87,7 @@ object FeatureFlag {
     const val OPEN_ICS_FILES = false
     const val APP_LINKS = true
     const val ADD_EMAIL_NOTIFICATIONS = true
-    const val DELETE_CALENDAR = false
+    const val DELETE_CALENDAR = true
     const val CHANGE_CALENDAR_SIMPLE_EVENT = true
     const val USE_EVENT_DECRYPTOR = true
     const val USE_EVENT_MANAGER = true
