d7727eadf59ca2545f7dd6b60f923bebcfe512d8
Ben <garciabenjamin@protonmail.com>
2022-03-10 11:33:11 +0100

    CALAND-1804: Set newly created calendar as default if there are no other active user calendars

--- app/src/main/java/me/proton/android/calendar/common/ServiceLocator.kt
+++ app/src/main/java/me/proton/android/calendar/common/ServiceLocator.kt
@@ -95,7 +95,7 @@ val viewModelModule = module {
     }
     viewModel<EventViewModel> { EventViewModel(get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get()) }
     viewModel<AccountViewModel> { AccountViewModel(get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get()) }
-    viewModel<CalendarFormViewModel> { CalendarFormViewModel(get(), get(), get(), get(), get(), get(), get(), get(), get(), get()) }
+    viewModel<CalendarFormViewModel> { CalendarFormViewModel(get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get()) }
 }
 
 val useCaseModule = module {
