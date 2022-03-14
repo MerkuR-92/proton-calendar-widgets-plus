2d9bfd770b9bf588a821d4b403865d9ad5b3df27
Ben <garciabenjamin@protonmail.com>
2022-03-14 14:46:39 +0100

    CALAND-1768: Scroll to current time when opening today in day view

--- app/src/main/java/me/proton/android/calendar/presentation/main/MainActivity.kt
+++ app/src/main/java/me/proton/android/calendar/presentation/main/MainActivity.kt
@@ -495,6 +495,11 @@ class MainActivity : AppCompatActivity(), KoinComponent {
                 } else if (showDayIntent != null && showDayIntent.data != null) {
 
                     val dayToShow = showDayIntent.data?.getQueryParameter("date")?.let { LocalDate.parse(it) }
+                    dayToShow?.let {
+                        lifecycleScope.launch {
+                            calendarViewModel.setJumpToCurrentTimeIfNeeded(it)
+                        }
+                    }
                     safeNavigateToMonth(dayToShow)
                 } else {
                     val openIcsIntent = mainViewModel.consumeIntent(INVITE_PROTON_INTENT_ACTION)
@@ -1095,6 +1100,21 @@ class MainActivity : AppCompatActivity(), KoinComponent {
         super.onDestroy()
         if (this::updateCalendarsJob.isInitialized && updateCalendarsJob.isActive) {
             updateCalendarsJob.cancel()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Check if we the app is opened on Day view and selected date is today.
+        //  If it is, then make it scroll to current time.
+        if (calendarViewModel.initialised.value == true) {
+            val selectedDate = calendarViewModel.selectedDate.value
+            if (calendarViewModel.viewMode.value == ViewMode.DAY && selectedDate != null && selectedDate == LocalDate.now()) {
+                lifecycleScope.launch {
+                    calendarViewModel.setJumpToCurrentTimeIfNeeded(selectedDate)
+                }
+            }
         }
     }
 }
