64a88c000cbd69efa190091ca424789db39b5094
bgarcia <bgarcia@proton.ch>
2022-04-25 11:00:08 +0200

    CALAND-1810: Updated welcome to new brand dialog

--- app/src/main/java/me/proton/android/calendar/common/utils/SpotlightUtils.kt
+++ app/src/main/java/me/proton/android/calendar/common/utils/SpotlightUtils.kt
@@ -1,22 +1,20 @@
 package me.proton.android.calendar.common.utils
 
 import android.content.Context
-import android.content.Intent
 import android.content.res.Resources
-import android.net.Uri
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import androidx.preference.PreferenceManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.android.synthetic.main.dialog_spotlight.view.*
-import kotlinx.android.synthetic.main.dialog_spotlight_v5.view.dialog_spotlight_v5_learn_more_button
+import kotlinx.android.synthetic.main.dialog_spotlight.view.dialog_spotlight_description
+import kotlinx.android.synthetic.main.dialog_spotlight.view.dialog_spotlight_title
 import kotlinx.android.synthetic.main.dialog_spotlight_v5.view.dialog_spotlight_v5_description
 import kotlinx.android.synthetic.main.dialog_spotlight_v5.view.dialog_spotlight_v5_got_it_button
 import kotlinx.android.synthetic.main.dialog_spotlight_v5.view.dialog_spotlight_v5_title
 import me.proton.android.calendar.BuildConfig
 import me.proton.android.calendar.R
 import me.proton.android.calendar.common.FeatureFlag.SPOTLIGHT
-import me.proton.android.calendar.common.REBRANDING_UPDATED_PROTON_URL
 import me.proton.android.calendar.common.SPOTLIGHT_VERSION_CODES
 import me.proton.android.calendar.common.SharedPreferencesKeys
 import me.proton.android.calendar.common.utils.AndroidUtils.setOnSingleClickListener
@@ -34,17 +32,17 @@ object SpotlightUtils {
         editor.apply()
     }
 
-    private fun Resources.getMonthViewDialogContent(): Pair<String, String> {
+    private fun Resources.getMonthViewDialogContent(): Pair<Int, Int> {
         return Pair(
-            this.getString(R.string.spotlight_dialog_month_view_title),
-            this.getString(R.string.spotlight_dialog_month_view_description)
+            R.string.spotlight_dialog_month_view_title,
+            R.string.spotlight_dialog_month_view_description
         )
     }
 
-    private fun Resources.getRebrandingDialogContent(): Pair<String, String> {
+    private fun Resources.getRebrandingDialogContent(): Pair<Int, Int> {
         return Pair(
-            this.getString(R.string.spotlight_v5_dialog_rebranding_title),
-            this.getString(R.string.spotlight_v5_dialog_rebranding_description)
+            R.string.spotlight_v5_dialog_rebranding_title,
+            R.string.spotlight_v5_dialog_rebranding_description
         )
     }
 
@@ -81,8 +79,8 @@ object SpotlightUtils {
     }
 
     private fun Context.displaySpotlightDialog(
-        title: String,
-        description: String
+        title: Int,
+        description: Int
     ) {
         val materialDialogBuilder = MaterialAlertDialogBuilder(this)
             .setCancelable(true)
@@ -97,16 +95,19 @@ object SpotlightUtils {
         val view = LayoutInflater.from(this)
             .inflate(R.layout.dialog_spotlight, null, false)
 
-        view.dialog_spotlight_title.text = title
-        view.dialog_spotlight_description.text = description
+        // Support link in text with getText
+        view.dialog_spotlight_title.text = getText(title)
+        view.dialog_spotlight_description.text = getText(description)
+        // Support click on link in text
+        view.dialog_spotlight_description.movementMethod = LinkMovementMethod.getInstance()
 
         materialDialogBuilder.setView(view)
         materialDialogBuilder.show()
     }
 
     private fun Context.displayV5SpotlightDialog(
-        title: String,
-        description: String
+        title: Int,
+        description: Int
     ) {
         val materialDialogBuilder = MaterialAlertDialogBuilder(this)
             .setCancelable(true)
@@ -118,16 +119,14 @@ object SpotlightUtils {
         val view = LayoutInflater.from(this)
             .inflate(R.layout.dialog_spotlight_v5, null, false)
 
-        view.dialog_spotlight_v5_title.text = title
-        view.dialog_spotlight_v5_description.text = description
+        // Support link in text with getText
+        view.dialog_spotlight_v5_title.text = getString(title)
+        view.dialog_spotlight_v5_description.text = getText(description)
+        // Support click on link in text
+        view.dialog_spotlight_v5_description.movementMethod = LinkMovementMethod.getInstance()
 
         var dialog: AlertDialog? = null
         view.dialog_spotlight_v5_got_it_button.setOnSingleClickListener {
-            dialog?.dismiss()
-        }
-        view.dialog_spotlight_v5_learn_more_button.setOnSingleClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(REBRANDING_UPDATED_PROTON_URL))
-            startActivity(browserIntent)
             dialog?.dismiss()
         }
 
