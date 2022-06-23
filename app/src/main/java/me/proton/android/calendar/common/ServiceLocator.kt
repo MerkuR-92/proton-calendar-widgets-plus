6cd6a14fde6172ae903aceabee7dee8beadccb91
Adam <adam.jodlowski@proton.ch>
2022-06-23 18:43:16 +0200

    CALAND-1999 Calculate local alarms for Subscribed Calendars using CalendarSettings

--- app/src/main/java/me/proton/android/calendar/common/ServiceLocator.kt
+++ app/src/main/java/me/proton/android/calendar/common/ServiceLocator.kt
@@ -109,7 +109,7 @@ val useCaseModule = module {
     factory<UpdateCalendarUseCase> { UpdateCalendarUseCase(get(), get(), get(), get()) }
     factory<SyncAlarmsUseCase> { SyncAlarmsUseCase(get(), get(), get(), get(), get()) }
     factory<HandleAlarmsUseCase> { HandleAlarmsUseCase(get(), get(), get(), get(), get()) }
-    factory<UpdateAlarmsUseCase> { UpdateAlarmsUseCase(get(), get(), get(), get(), get()) }
+    factory<UpdateAlarmsUseCase> { UpdateAlarmsUseCase(get(), get(), get(), get(), get(), get()) }
     factory<CreateCalendarUseCase> { CreateCalendarUseCase(get(), get(), get(), get(), get(), get(), get()) }
     factory<KeySetupUseCase> { KeySetupUseCase(get(), get(), get(), get(), get()) }
     factory<ResetCalendarsKeyUseCase> { ResetCalendarsKeyUseCase(get(), get(), get(), get(), get()) }
