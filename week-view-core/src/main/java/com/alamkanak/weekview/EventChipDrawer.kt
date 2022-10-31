4b776a54f24f2923b9ed1d15f0d741bbbed77573
Benjamin <bgarcia@proton.ch>
2022-10-31 13:13:36 +0100

    CALAND-2273: Hide start time of multi day part day events in 3 days and week views

--- week-view-core/src/main/java/com/alamkanak/weekview/EventChipDrawer.kt
+++ week-view-core/src/main/java/com/alamkanak/weekview/EventChipDrawer.kt
@@ -250,7 +250,7 @@ internal class EventChipDrawer(
         val bounds = eventChip.bounds
 
         val multiDayTimeText =
-            if (eventChip.event.isMultiDay && eventChip.event.isNotAllDay && ((viewState.isSingleDay && eventChip.index == 0) || !viewState.isSingleDay)) {
+            if (eventChip.event.isMultiDay && eventChip.event.isNotAllDay && viewState.isSingleDay && eventChip.index == 0) {
                 val hour = eventChip.event.startTime.hour
                 val minutes = eventChip.event.startTime.minute
                 viewState.timeFormatter(hour, minutes)
