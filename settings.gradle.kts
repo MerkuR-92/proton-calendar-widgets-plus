2d4bab80c5bc451dd38570261295c6a63869a95d
Neil Marietta <neil.marietta@protonmail.com>
2022-11-25 15:30:02 +0100

    Replaced Git Submodules by Include Core Build Gradle Plugin.

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "ProtonCalendar"
+
+plugins {
+    id("me.proton.core.gradle-plugins.include-core-build") version "1.1.1"
+}
+
+includeCoreBuild {
+    branch.set("main")
+    includeBuild("gopenpgp")
+}
+
+include(":app")
+include(":week-view-core")
