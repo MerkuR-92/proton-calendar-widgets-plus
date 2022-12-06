16faf23c1c6a61a868ca7fcbaf7843ddbc6dd4cb
Adam <adam.jodlowski@proton.ch>
2022-12-06 18:16:41 +0100

    CALAND-2021 Handle runtime permission for showing notifications

--- app/src/main/java/me/proton/android/calendar/common/Constants.kt
+++ app/src/main/java/me/proton/android/calendar/common/Constants.kt
@@ -139,6 +139,7 @@ object SharedPreferencesKeys {
     const val ALTERNATIVE_ROUTING = "alternative_routing"
     const val VIEW_MODE = "view_mode"
     const val SHOW_CONTACTS_PERMISSIONS_DIALOG = "show_contacts_permissions_dialog"
+    const val SHOW_NOTIFICATIONS_PERMISSIONS_DIALOG = "show_notifications_permissions_dialog"
     const val HACK_USER_ADDRESS_INVALID_FOR_SENDING = "hack_user_address_invalid_for_sending"
     const val LAST_SPOTLIGHT_SHOWN = "last_spotlight_shown"
     const val APP_SETTINGS_LANGUAGE = "app_settings_language"
