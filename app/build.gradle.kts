c2ad8d40068c96c3bbd6827a4c658e0fed3486c1
Benjamin <bgarcia@proton.ch>
2023-04-13 15:20:26 +0200

    2.9.0 spotlight
    Bump version to 2.9.0 (190)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -309,9 +309,9 @@ object Config {
     const val ndkVersion = "21.3.6528147"
     const val buildToolsVersion = "30.0.3"
     const val targetSdk = 33
-    const val versionCode = 189
+    const val versionCode = 190
     const val testInstrumentationRunner = "me.proton.android.calendar.uitest.HiltTestRunner"
-    const val versionName = "2.8.0"
+    const val versionName = "2.9.0"
     const val archivesBaseName = "ProtonCalendar-$versionName($versionCode)"
     val resourceConfigurations
         get() = listOf(
