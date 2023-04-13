1740de0714e9d7f7848c1410008209c77016990e
Benjamin <bgarcia@proton.ch>
2023-04-13 11:41:45 +0200

    CALAND-2420: Enable edit shared calendar settings feature flag.

--- app/src/main/java/me/proton/android/calendar/common/Constants.kt
+++ app/src/main/java/me/proton/android/calendar/common/Constants.kt
@@ -130,7 +130,7 @@ object FeatureFlag {
     const val THREE_DAYS_VIEW = true
     const val WEEK_VIEW = true
     const val DRAG_AND_DROP = false
-    const val EDITING_SHARED_CALENDARS = false
+    const val EDITING_SHARED_CALENDARS = true
     const val CLEAR_CALENDAR = false
     const val IMPORT_ICS = true
     const val SHOW_EVENT_SEARCH = false
