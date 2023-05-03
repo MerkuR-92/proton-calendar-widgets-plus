15be909d250ee16f8830dafa7083d9a87e9ffb22
Adam Jodlowski <adam.jodlowski@proton.ch>
2023-05-03 14:22:38 +0200

    Show encrypted search and bump version to 2.10.0 (192)

--- app/src/main/java/me/proton/android/calendar/common/Constants.kt
+++ app/src/main/java/me/proton/android/calendar/common/Constants.kt
@@ -98,7 +98,7 @@ const val EASY_SWITCH_VERSION_CODE = 150
 const val WEEK_VIEW_VERSION_CODE = 164
 const val IMPORT_VERSION_CODE = 186
 const val CALENDAR_PROVIDER_VERSION_CODE = 191
-const val SEARCH_VERSION_CODE = 1 // TODO fix when we unhide
+const val SEARCH_VERSION_CODE = 192
 
 const val FETCH_EVENTS_MAX_DAYS_WINDOW = 42 // Maximum number of days shown at once in a view (Currently month view)
 
@@ -109,7 +109,7 @@ val SPOTLIGHT_VERSION_CODES = arrayListOf(
     WEEK_VIEW_VERSION_CODE, // Week view (2.3.10)
     IMPORT_VERSION_CODE, // Import (2.6.6)
     CALENDAR_PROVIDER_VERSION_CODE, // Calendar provider (2.9.1)
-    SEARCH_VERSION_CODE // Search view (2.x.x) // TODO fix when we unhide
+    SEARCH_VERSION_CODE // Search view (2.10.0)
 )
 
 object FeatureFlag {
@@ -135,7 +135,7 @@ object FeatureFlag {
     const val EDITING_SHARED_CALENDARS = true
     const val CLEAR_CALENDAR = false
     const val IMPORT_ICS = true
-    const val SHOW_EVENT_SEARCH = false
+    const val SHOW_EVENT_SEARCH = true
 }
 
 object HttpResponseCode {
