49869f4851587206773d17a99e4553aafaa8b533
Benjamin <bgarcia@proton.ch>
2023-05-31 17:52:15 +0200

    Removed default value from feature flags enum

--- app/src/main/java/me/proton/android/calendar/common/utils/CalendarFeatureFlag.kt
+++ app/src/main/java/me/proton/android/calendar/common/utils/CalendarFeatureFlag.kt
@@ -2,7 +2,7 @@ package me.proton.android.calendar.common.utils
 
 import me.proton.core.featureflag.domain.entity.FeatureId
 
-enum class CalendarFeatureFlag(val featureId: FeatureId, val defaultLocalValue: Boolean = false, val isLocalFlag: Boolean = true) {
+enum class CalendarFeatureFlag(val featureId: FeatureId, val defaultLocalValue: Boolean, val isLocalFlag: Boolean) {
 
     // Naming convention for remote feature flags:
     //  - Features shared among different products: {FeatureName}{Platform}{Product} (ex: RatingAndroidCalendar)
@@ -13,27 +13,27 @@ enum class CalendarFeatureFlag(val featureId: FeatureId, val defaultLocalValue: 
 
     // Local only flag (unknown to remote API)
     // Enabled
-    AddAttendees(FeatureId("AddAttendees"), true),
-    ChangeAnswer(FeatureId("ChangeAnswer"), true),
-    OpenInvitation(FeatureId("OpenInvitation"), true),
-    AppLinks(FeatureId("AppLinks"), true),
-    DeleteCalendar(FeatureId("DeleteCalendar"), true),
-    ChangeCalendarSimpleEvent(FeatureId("ChangeCalendarSimpleEvent"), true),
-    UseEventDecryptor(FeatureId("UseEventDecryptor"), true),
-    ChangeLanguage(FeatureId("ChangeLanguage"), true),
-    MonthView(FeatureId("MonthView"), true),
-    Feedback(FeatureId("Feedback"), true),
-    AutoInvitesSetting(FeatureId("AutoInvitesSetting"), true),
-    ImportAssistant(FeatureId("ImportAssistant"), true),
-    ThreeDaysView(FeatureId("ThreeDaysView"), true),
-    WeekView(FeatureId("WeekView"), true),
-    ImportIcs(FeatureId("ImportIcs"), true),
-    EditingSharedCalendars(FeatureId("EditingSharedCalendars"), true),
-    ShowEventSearch(FeatureId("ShowEventSearch"), true),
+    AddAttendees(FeatureId("AddAttendees"), true, true),
+    ChangeAnswer(FeatureId("ChangeAnswer"), true, true),
+    OpenInvitation(FeatureId("OpenInvitation"), true, true),
+    AppLinks(FeatureId("AppLinks"), true, true),
+    DeleteCalendar(FeatureId("DeleteCalendar"), true, true),
+    ChangeCalendarSimpleEvent(FeatureId("ChangeCalendarSimpleEvent"), true, true),
+    UseEventDecryptor(FeatureId("UseEventDecryptor"), true, true),
+    ChangeLanguage(FeatureId("ChangeLanguage"), true, true),
+    MonthView(FeatureId("MonthView"), true, true),
+    Feedback(FeatureId("Feedback"), true, true),
+    AutoInvitesSetting(FeatureId("AutoInvitesSetting"), true, true),
+    ImportAssistant(FeatureId("ImportAssistant"), true, true),
+    ThreeDaysView(FeatureId("ThreeDaysView"), true, true),
+    WeekView(FeatureId("WeekView"), true, true),
+    ImportIcs(FeatureId("ImportIcs"), true, true),
+    EditingSharedCalendars(FeatureId("EditingSharedCalendars"), true, true),
+    ShowEventSearch(FeatureId("ShowEventSearch"), true, true),
 
     // Disabled
-    Subscription(FeatureId("Subscription"), false),
-    ShowSignatureVerificationBadges(FeatureId("ShowSignatureVerificationBadges"), false),
-    DragAndDrop(FeatureId("DragAndDrop"), false),
-    ClearCalendar(FeatureId("ClearCalendar"), false)
+    Subscription(FeatureId("Subscription"), false, true),
+    ShowSignatureVerificationBadges(FeatureId("ShowSignatureVerificationBadges"), false, true),
+    DragAndDrop(FeatureId("DragAndDrop"), false, true),
+    ClearCalendar(FeatureId("ClearCalendar"), false, true)
 }
