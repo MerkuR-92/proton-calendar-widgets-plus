47ec27bc27be92f9198b359181953c48c44ced36
Benjamin <bgarcia@proton.ch>
2023-07-17 14:47:48 +0200

    Updated holiday calendar spotlight version code

--- app/src/main/java/me/proton/android/calendar/common/Constants.kt
+++ app/src/main/java/me/proton/android/calendar/common/Constants.kt
@@ -103,7 +103,7 @@ const val WEEK_VIEW_VERSION_CODE = 164
 const val IMPORT_VERSION_CODE = 186
 const val CALENDAR_PROVIDER_VERSION_CODE = 191
 const val SEARCH_VERSION_CODE = 1 // TODO disable search spotlight
-const val HOLIDAY_CALENDAR_VERSION_CODE = 216
+const val HOLIDAY_CALENDAR_VERSION_CODE = 217
 
 const val FETCH_EVENTS_MAX_DAYS_WINDOW = 42 // Maximum number of days shown at once in a view (Currently month view)
 
