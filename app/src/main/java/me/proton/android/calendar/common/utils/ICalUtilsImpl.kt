9453173b57eb71b3cb29de4abcdb13981fb53459
Benjamin <bgarcia@proton.ch>
2024-02-08 17:14:10 +0100

    Support ex dates in calendar parts.

--- app/src/main/java/me/proton/android/calendar/common/utils/ICalUtilsImpl.kt
+++ app/src/main/java/me/proton/android/calendar/common/utils/ICalUtilsImpl.kt
@@ -393,6 +393,7 @@ object ICalUtilsImpl : ICalUtils {
                 // We use setProperty to avoid having duplicates, but we need to use addProperty for Attendees
                 // to properly add multiple ones
                 if (iCalProperty::class == Attendee::class) left.events.first().addProperty(iCalProperty)
+                else if (iCalProperty::class == ExceptionDates::class) left.events.first().addProperty(iCalProperty)
                 else if (iCalProperty::class == DateTimeStamp::class) {
                     // Take latest DateTimeStamp
                     val leftDateTimeStamp = left.events.first().getProperty(DateTimeStamp::class.java)?.value
