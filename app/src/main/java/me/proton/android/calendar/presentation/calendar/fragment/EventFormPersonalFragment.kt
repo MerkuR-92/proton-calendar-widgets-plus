f4e37dd1373ae578bf2fd9934cbc98bef0cc5174
Benjamin <bgarcia@proton.ch>
2024-04-23 16:20:00 +0200

    Fixed call to Event displayColor following rebase.

--- app/src/main/java/me/proton/android/calendar/presentation/calendar/fragment/EventFormPersonalFragment.kt
+++ app/src/main/java/me/proton/android/calendar/presentation/calendar/fragment/EventFormPersonalFragment.kt
@@ -320,15 +320,16 @@ class EventFormPersonalFragment() : BaseDialogFragment<FragmentEventFormPersonal
             val isColorPerEventEnabled = featureFlagViewModel.isColorPerEventEnabled()
             binding.eventFormPersonalColorLayout.visibleOrGone(isColorPerEventEnabled)
             if (isColorPerEventEnabled) {
+                val isFreeUser = eventViewModel.user.hasSubscriptionForMail().not()
                 binding.eventFormPersonalColor.text = ColorUtils.getColorNameForHex(
                     resources.getStringArray(R.array.colors_with_names),
-                    event.displayColor
+                    event.getDisplayColor(isFreeUser)
                 ) ?: getString(R.string.undefined_color)
-                binding.eventFormPersonalColorDefault.visibleOrGone(event.displayColor == event.calendar.color)
+                binding.eventFormPersonalColorDefault.visibleOrGone(event.getDisplayColor(isFreeUser) == event.calendar.color)
 
                 ImageViewCompat.setImageTintList(
                     binding.eventFormPersonalColorIcon,
-                    ColorStateList.valueOf(Color.parseColor(event.displayColor))
+                    ColorStateList.valueOf(Color.parseColor(event.getDisplayColor(isFreeUser)))
                 )
             }
         })
@@ -384,7 +385,7 @@ class EventFormPersonalFragment() : BaseDialogFragment<FragmentEventFormPersonal
                         requireContext(),
                         resources.getString(R.string.dialog_title_event_color_picker),
                         resources.getStringArray(R.array.colors_with_names),
-                        eventViewModel.eventLiveData.value!!.displayColor,
+                        eventViewModel.eventLiveData.value!!.getDisplayColor(isFreeUser),
                         eventViewModel.eventLiveData.value!!.calendar.color
                     ) {
                         eventViewModel.handleColor(it)
