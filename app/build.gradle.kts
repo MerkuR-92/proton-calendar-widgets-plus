a26aecd1596e6af9fa4ebc3f059a4b92bc8bd9cf
Adam Jodlowski <adam.jodlowski@proton.ch>
2024-12-06 10:18:50 +0100

    Bump version to 2.21.11 (297)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -368,9 +368,9 @@ object Config {
     const val ndkVersion = "21.3.6528147"
     const val buildToolsVersion = "34.0.0"
     const val targetSdk = 34
-    const val versionCode = 296
+    const val versionCode = 297
     const val testInstrumentationRunner = "me.proton.android.calendar.uitest.extension.HiltTestRunner"
-    const val versionName = "2.21.10"
+    const val versionName = "2.21.11"
     const val archivesBaseName = "ProtonCalendar-$versionName($versionCode)"
     val resourceConfigurations
         get() = listOf(
