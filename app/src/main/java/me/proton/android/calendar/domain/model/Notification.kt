b2816031ef68a36916909c26475f2ff55d85a5d6
Adam Jodlowski <adam.jodlowski@proton.ch>
2025-03-10 15:56:24 +0100

    Override equals method to make Notification class compare itself properly

--- app/src/main/java/me/proton/android/calendar/domain/model/Notification.kt
+++ app/src/main/java/me/proton/android/calendar/domain/model/Notification.kt
@@ -16,6 +16,21 @@ sealed class Notification(val trigger: Trigger) {
             else -> null
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Notification
+
+        if (trigger != other.trigger) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return trigger.hashCode()
+    }
 }
 
 /**
