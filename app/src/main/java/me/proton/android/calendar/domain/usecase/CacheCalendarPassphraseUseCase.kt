e74808dfb0788ba00d13b0b6d335a80692f4d085
Adam Jodlowski <adam.jodlowski@proton.ch>
2025-05-28 13:08:35 +0200

    Verify signature of Member's Passphrase with all available Member's AddressKeys
    CALAND-3060

--- app/src/main/java/me/proton/android/calendar/domain/usecase/CacheCalendarPassphraseUseCase.kt
+++ app/src/main/java/me/proton/android/calendar/domain/usecase/CacheCalendarPassphraseUseCase.kt
@@ -47,12 +47,21 @@ class CacheCalendarPassphraseUseCase @Inject constructor( // TODO TEST
         // decrypt CalendarPassphrase -- actually a Passphrase for CalendarKey
         // AddressKey used to d/encrypt Passphrase for this Member might not be the primary AddressKey
         val plaintextPassphrase = memberAddress.useKeys(cryptoContext) {
+
+            val publicKeysForVerification = this.publicKeyRing.keys.map { it.key }
+
             this.privateKeyRing.keys.firstNotNullOfOrNull { privateKey ->
                 val decryptedPassphrase = privateKey.unlockOrNull(cryptoContext)?.decryptTextOrNull(cryptoContext, memberPassphrase.passphrase)
 
                 if (decryptedPassphrase != null) {
                     val isKeyCompromised = !privateKey.canVerify
-                    val isSignatureValid = cryptoContext.pgpCrypto.verifyText(decryptedPassphrase, memberPassphrase.signature, cryptoContext.pgpCrypto.getPublicKey(privateKey.key))
+                    val isSignatureValid = publicKeysForVerification.any {
+                        cryptoContext.pgpCrypto.verifyText(
+                            decryptedPassphrase,
+                            memberPassphrase.signature,
+                            it
+                        )
+                    }
 
                     // this is a special case, when AddressKey is compromised, we should allow for decrypting
                     //  the CalendarPassphrase and ignore verification error (because verification will always fail),
