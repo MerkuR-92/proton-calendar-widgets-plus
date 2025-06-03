66d0349b3ac8ae16b45bb68729924ea0eaa9c7c5
Adam Jodlowski <adam.jodlowski@proton.ch>
2025-06-03 10:05:50 +0200

    Stop ignoring compromised keys for Passphrase signature verification

--- app/src/main/java/me/proton/android/calendar/domain/usecase/CacheCalendarPassphraseUseCase.kt
+++ app/src/main/java/me/proton/android/calendar/domain/usecase/CacheCalendarPassphraseUseCase.kt
@@ -54,7 +54,6 @@ class CacheCalendarPassphraseUseCase @Inject constructor( // TODO TEST
                 val decryptedPassphrase = privateKey.unlockOrNull(cryptoContext)?.decryptTextOrNull(cryptoContext, memberPassphrase.passphrase)
 
                 if (decryptedPassphrase != null) {
-                    val isKeyCompromised = !privateKey.canVerify
                     val isSignatureValid = publicKeysForVerification.any {
                         cryptoContext.pgpCrypto.verifyText(
                             decryptedPassphrase,
@@ -63,10 +62,7 @@ class CacheCalendarPassphraseUseCase @Inject constructor( // TODO TEST
                         )
                     }
 
-                    // this is a special case, when AddressKey is compromised, we should allow for decrypting
-                    //  the CalendarPassphrase and ignore verification error (because verification will always fail),
-                    //  but trust the signature verification only when key is not compromised
-                    if ((isKeyCompromised) || isSignatureValid) decryptedPassphrase else null
+                    if (isSignatureValid) decryptedPassphrase else null
                 } else null
             }
         }
