ea1aabe781454115ac581a83ffbed3b679492077
Adam Jodlowski <adam.jodlowski@proton.ch>
2025-07-02 15:45:44 +0200

    Bump targetSDK to 35
    CALAND-3088

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -370,7 +370,7 @@ object Config {
     const val minSdk = 23
     const val ndkVersion = "21.3.6528147"
     const val buildToolsVersion = "34.0.0"
-    const val targetSdk = 34
+    const val targetSdk = 35
     const val versionCode = 322
     const val testInstrumentationRunner = "me.proton.android.calendar.uitest.extension.HiltTestRunner"
     const val versionName = "2.26.0"
