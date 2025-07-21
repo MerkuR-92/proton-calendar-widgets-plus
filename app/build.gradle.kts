fff0efb8e91ff3b1faf872a4f0bfea59d32cf011
Niccolò Forlini <niccolo.forlini@proton.ch>
2025-07-21 16:30:26 +0200

    Bump versionCode to 325
    CALAND-3049

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -375,7 +375,7 @@ object Config {
     const val ndkVersion = "21.3.6528147"
     const val buildToolsVersion = "34.0.0"
     const val targetSdk = 35
-    const val versionCode = 324
+    const val versionCode = 325
     const val testInstrumentationRunner = "me.proton.android.calendar.uitest.extension.HiltTestRunner"
     const val versionName = "2.27.0"
     const val archivesBaseName = "ProtonCalendar-$versionName($versionCode)"
