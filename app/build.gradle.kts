8037aefe01b16b74e949ef213693b48b3cbd11df
Rok Oblak <rok.oblak@external.proton.ch>
2025-08-18 13:16:22 +0200

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -393,7 +393,7 @@ object Config {
     const val ndkVersion = "21.3.6528147"
     const val buildToolsVersion = "34.0.0"
     const val targetSdk = 35
-    const val versionCode = 328
+    const val versionCode = 329
     const val testInstrumentationRunner = "me.proton.android.calendar.uitest.extension.HiltTestRunner"
     const val versionName = "2.28.0"
     const val archivesBaseName = "ProtonCalendar-$versionName($versionCode)"
